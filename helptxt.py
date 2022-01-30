700b97bfadb143d59b752011d50f3cb04d7b3f6a
Bartosz Laskowski <56258631+bartolek@users.noreply.github.com>
2022-01-30 21:49:55 +0100

    Updated help command

--- helptxt.py
+++ helptxt.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="$")
-client.remove_command("help")
+bot = commands.Bot(command_prefix="$")
+bot.remove_command("help")
 
-@client.command()
+@bot.command()
 async def help(ctx):
-    embed=discord.Embed(title="===== help ====", description="Dostępne komendy:", color=0xc41c1c)
-    embed.add_field(name="Włączenie bota goomba", value="$goomba", inline=False)
-    await ctx.author.send(embed=embed)
+    embed=discord.Embed(title="HELME", description="Dostępne komendy:", color=0xca2b2b)
+    embed.add_field(name="$goomba {Argument}", value="Argumenty: status/ON/OFF", inline=False)
+    embed.add_field(name="$play {Nazwa}", value="Zmiana statusu bota, W grze:", inline=True)
+    embed.add_field(name="$listen {Nazwa}", value="Zmiana statusu bota, Słucha:", inline=True)
+    embed.add_field(name="$randomizer {Arg} {Arg}", value="Losuje liczbę z przedziału ", inline=False)
+    await ctx.send(embed=embed)
