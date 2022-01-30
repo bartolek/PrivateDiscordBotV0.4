16940a8c8b206d2218dad2fd03ed22c3af3d5544
Bartosz Laskowski <56258631+bartolek@users.noreply.github.com>
2022-01-30 20:38:30 +0100

    Added goomba

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ from discord.ext import commands
 import helptxt
 import random
 
-client = commands.Bot(command_prefix="$")
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+
+client = commands.Bot(command_prefix="$", intents=intents)
 
 # Powiadomienie o poprawnym uruchomieniu bota
 @client.event
@@ -35,7 +39,38 @@ async def randomizer(ctx, min : int, max : int):
         await ctx.channel.send(numer)
     else:
         await ctx.channel.send("Pierwsza podana liczba musi być mniejsza od drugiej!")
+
+currentStatus = "ON"
+
+@client.command()
+async def goomba(ctx, input : str):
+    global currentStatus
+    if input == "status":
+        await ctx.channel.send(f"Aktualny status goomby to: {currentStatus}")    
+    elif input == "ON" or input == "OFF":
+        currentStatus = input
+        await ctx.channel.send(f"Zmieniłeś status goomby na: {currentStatus}")
+    else:
+        await ctx.channel.send("Poprawne użycie komendy to: $goomba status/ON/OFF")
+
     
+
+@client.listen('on_message')
+async def on_messages(message):
+    if message.attachments:
+        if currentStatus == "ON":
+            emojiA = '\U0001F1E6'
+            emojiB = '\U0001F1E7'
+            emojiC = '\U0001F1E8'
+            emojiD = '\U0001F1E9'
+            await message.add_reaction(emojiA)
+            await message.add_reaction(emojiB)
+            await message.add_reaction(emojiC)
+            await message.add_reaction(emojiD)
+            await client.process_commands(message)
+
+    
+
 
 
 
@@ -44,5 +79,6 @@ async def randomizer(ctx, min : int, max : int):
 #play nazwagry
 #listen nazwapiosenki
 #randomizer min max
+#goomba status/on/off
 
 client.run("OTM3MDg4NTQ5NDQ1MDYyNzQ3.YfWpuA.87Lo-nPE0dKYyTGN7S6V4W58YU0")
