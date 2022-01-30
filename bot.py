89d117db0518185d3f523c9736529a558f801211
Bartosz Laskowski <56258631+bartolek@users.noreply.github.com>
2022-01-30 18:41:42 +0100

    Added command randomizer

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from unicodedata import name
 import discord
 from discord.ext import commands 
 import helptxt
+import random
 
 client = commands.Bot(command_prefix="$")
 
@@ -26,6 +27,22 @@ async def play(ctx, gamename):
 async def listen(ctx, songname):
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=songname))
 
+#Losuj liczbe w przedziale
+@client.command()
+async def randomizer(ctx, min : int, max : int):
+    if min < max:
+        numer = random.randrange(min, max)
+        await ctx.channel.send(numer)
+    else:
+        await ctx.channel.send("Pierwsza podana liczba musi być mniejsza od drugiej!")
+    
 
+
+
+#lista aktualnych komend
+#help
+#play nazwagry
+#listen nazwapiosenki
+#randomizer min max
 
 client.run("OTM3MDg4NTQ5NDQ1MDYyNzQ3.YfWpuA.87Lo-nPE0dKYyTGN7S6V4W58YU0")
